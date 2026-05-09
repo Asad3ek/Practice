b29110cd6304a45cb8a6322ca9b20d0909b57b84
Anthony <asadbeksaydullayev2911@gmail.com>
2026-05-09 15:30:18 +0900

    feat: Learn Primitive Variables & Dunders(meth,state)

---
+++ primitive_var.py
@@ -0,0 +1,50 @@
+print("=========Primitive Variables=========")
+''' String
+    Number
+    Boolean 
+    Null
+    Undefined
+'''
+# VARIABLES in JavaScript  - name of the storage location deyiladi
+
+# VARIABLES In Python - name of the reference!
+# yani hamma narsa obj bo'lganligi sababli reference ning nomlanishi deyiladi.
+
+print("====Number variables====")
+
+count = 47
+count_type = type(count)
+print(f"The count: {count}, The type of count: {count_type}")
+
+
+print("====String variables====")
+course = "AI PYTHON fullstack "
+result = type(course)
+result1 = course.title()
+print(f"Course Title: {result1}, The type of course title: {result}")
+
+result = course.replace("AI", "Artificial Intellegence").upper()
+print(result)
+
+# bu method lar boshlang'ich kiritilgan datani saqlagan holda o'zgartiradi
+
+
+print("====Boolean variables====")
+# function > input(), str(), bool(), int()
+Z = input("Enter your value for Z ")
+print("Z:", Z)
+
+result = Z.isnumeric()
+print(f"The Input Value is Numeric: {result}")
+
+# TRUTHY >> true,  " ", number(100), string(abc)
+# FALTHY >> 0, "", false, none
+
+
+test_falsy = ""
+result1 = bool(test_falsy)
+print("result1:", result1)
+
+test_truthy = 100
+result2 = bool(test_truthy)
+print("result2:", result2)
--- run.py
+++ run.py
@@ -1,7 +1,24 @@
-a = 20
-print("a:", a)
+print("PYTHON LANGUAGE STARTED")
 
-print("====$$$========$$$$======$$$========$$$")
+# PRIMATIVE Variables
+'''
+Primitive variables - JavaScriptda value ga ega ammo 
+refernce ga ega emas.
 
-text = "Hello By PYTHON"
-print(text)
+PYTHON da - hamma narsa object hisoblanadi va shu sababli
+reference ga ega. 
+'''
+
+# DUNDER - bu double underscore __ degani va PYTHON ni ichki qurulish mehanizmi hisoblanadi.
+# DUNDER: __builtins__ >> systeam variable hisoblansa
+# DUNDER: __init__ >> maxsus Method i hisoblanadi
+
+'''PYTHON builtins Tool lari :
+1. TYPES: >> int, float, string, list, dict
+2. FUNCTION: >> print(), len(), input(), type() str(), int(), input(), bool() 
+3. CONSTANTS: True, False, None
+'''
+# dir(....) - agar dunderlarning ichki tarkibini
+# ko'rish uchun foydalanadi.
+
+print(dir(__builtins__))
