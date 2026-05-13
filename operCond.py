69753a5358b49d3631af7e5dcb14cccd5e3676ff
Anthony <asadbeksaydullayev2911@gmail.com>
2026-05-13 22:45:33 +0900

    Feat: Learn Condition and Operators In Python

---
+++ operCond.py
@@ -0,0 +1,27 @@
+print("====== Operators ======")
+
+a = 10
+b = 5
+
+print(a / b)
+print(a * b)
+print(a + b)
+
+print(a // b) # butun qismini qaytaradi
+print(a % b) #qoldiq qismini qaytaradi.
+print(a **2) # kvadrarini qaytaradi
+print(a **3) # kubini yani 3 darajasini qaytaradi
+
+age = 20 
+
+person = None
+
+if age > 18:
+    person = "Adult" 
+else:
+    person = "Minor"
+
+print("Person:", person)
+
+person2 = "Adult" if age > 18 else "Minor"
+print("Person2:", person2)
