f11e54cd049b6f7a960f072ffe172d231fb1e194
Anthony <asadbeksaydullayev2911@gmail.com>
2026-05-17 18:52:57 +0900

    feat: Learn comperhensions: set | dictionary | list

--- array_set.py
+++ array_set.py
@@ -4,8 +4,8 @@
 
 '''
 
+from array import array
 print("======= ARRAY =====")
-from array import array 
 numbers = array("i", [23, 3, 67, 83, 4, 68, 89, 0])
 print("Numbers list1: ", numbers)
 
@@ -19,7 +19,8 @@ print("Numbers list3: ", numbers)
 
 print("======= Set =====")
 # { SET } - bu arraydafi bir xil bo'lgan malumotlarning faqat bittasini oladi.
-database = array("i", [12, 3, 4, 2, 3, 14, 7, 8, 3, 45, 5, 23, 34, 34 ,6 ,76, 78, 7, 77, 75, 4, 36, 7, 7, 3, 6, 54])
+database = array("i", [12, 3, 4, 2, 3, 14, 7, 8, 3, 45, 5,
+                 23, 34, 34, 6, 76, 78, 7, 77, 75, 4, 36, 7, 7, 3, 6, 54])
 print("Database1:", database)
 new_database = set(database)
 print("Database2:", new_database)
@@ -28,15 +29,19 @@ print("Database2:", new_database)
 
 print("======= Spesific operators with Set =====")
 # | & - ^
-numb1 = {77, 45 , 7, 14, 77}
-numb2 = {77, 45 , 10, 11}
+numb1 = {77, 45, 7, 14, 77}
+numb2 = {77, 45, 10, 11}
 
-result1 = numb1 | numb2       #ikkita setni bitta qilib beradi takrorlashlarsiz => result1: {7, 10, 11, 45, 77, 14}
+result1 = numb1 | numb2
+# ikkita setni bitta qilib beradi takrorlashlarsiz => result1: {7, 10, 11, 45, 77, 14}
 print("result1:", result1)
 
 result2 = numb1 & numb2
-print("result2:", result2)   #ikkita setda mavjud bir xil sonlarni olib beradi => result2: {77, 45}
+# ikkita setda mavjud bir xil sonlarni olib beradi => result2: {77, 45}
+print("result2:", result2)
 
 result3 = numb1 ^ numb2
-print("result3:", result3)   #ikkita setda ham mavjud bo'lgan sonlarni tashlab yuboradi qolganlarni bitta arrayda 
-                             #set qilib beradi => result3: {7, 10, 11, 14}
+# ikkita setda ham mavjud bo'lgan sonlarni tashlab yuboradi qolganlarni bitta arrayda
+print("result3:", result3)
+
+# set qilib beradi => result3: {7, 10, 11, 14}
